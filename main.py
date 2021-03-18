9b9ba5e6890446118e978cec2a3cc78560ecb1c6
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-03-18 18:21:56 +0100

    Implement database

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify, redirect, request
+from flask import Flask, jsonify, redirect, request, g
 from flask_cors import CORS
-
+import configparser
 import sqlite3
 
 app = Flask(__name__)
@@ -13,6 +13,23 @@ test = {
             "balance": 9000,
             "lastUpdate": 1614867820842
         }
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+DATABASE = config['database']['file']
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def index():
