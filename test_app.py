a2d7b90c824bd3c7f0ae808d9250f79dc63c75e6
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-03-23 15:55:23 +0100

    Lint test case

--- test_app.py
+++ test_app.py
@@ -4,12 +4,12 @@ from main import app
 
 
 def test_start():
-    response = app.test_client().get('/api')
+    response = app.test_client().get("/api")
 
     assert response.status_code == 200
 
 
 def test_read():
-    response = app.test_client().get('/api')
+    response = app.test_client().get("/api")
     data = json.loads(response.data.decode())
     assert isinstance(data, list)
