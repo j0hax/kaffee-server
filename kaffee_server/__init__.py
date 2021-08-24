c699f9a49bb1000a0be0c9dde99c6c4c3c14ca8f
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-08-24 17:04:37 +0200

    Remove unused config

--- kaffee_server/__init__.py
+++ kaffee_server/__init__.py
@@ -39,13 +39,6 @@ def create_app(test_config=None):
         BEANINFO={"brand": "Tchibo", "type": "Espresso, Mailänder Art"},
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
