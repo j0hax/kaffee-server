c12de5e519ec9c53650e72b8f4c09627b0949226
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-12-14 10:53:27 +0100

    Remove unused code

--- kaffee_server/api/__init__.py
+++ kaffee_server/api/__init__.py
@@ -29,21 +29,3 @@ def to_camel_case(snake: str) -> str:
 @bp.route("/")
 def entry():
     return redirect(url_for(".version1.api"))
-
-
-"""
-@bp.app_errorhandler(404)
-@bp.app_errorhandler(500)
-def handle_api(error):
-    if isinstance(error, HTTPException):
-        response = {
-            "code": error.code,
-            "name": error.name,
-            "description": error.description,
-            "url": request.path,
-            "type": str(type(error)),
-            "time": time(),
-            "id": uuid4(),
-        }
-        return jsonify(response), error.code
-"""
