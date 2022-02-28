46d2e7f7f38cd425e2bce9f244c660fc915fa8a5
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2022-02-28 13:26:32 +0100

    Manually specify port

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 import json
+import sys
 import bjoern
 from kaffee_server import create_app
 from kaffee_server.maintenance import scheduler
@@ -12,4 +13,10 @@ if __name__ == "__main__":
     scheduler.init_app(app)
     scheduler.start()
 
-    bjoern.run(app, "0.0.0.0", 5000)
+    port = 5000
+
+    print(f"Starting on Port {port}")
+
+    sys.stdout.flush()
+
+    bjoern.run(app, "0.0.0.0", port)
